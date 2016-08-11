70a3594dcc22cd463af7404125a430e3cf5a9bb6
Milica Ninković <Milica Ninkovi>
2016-08-11 14:46:12 +0200

    Inicijalizacija

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/* Config fajl za konekciju sa bazom i
+definisanje baze podataka */
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');           /*Prazno polje za lozinku, kasnije dodati*/
+define('DB_NAME','labvezbe');   /*Ime baze podataka*/
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: milica
+ * Date: 11.8.2016.
+ * Time: 13.50
+ */
+$status="Niste prijavljeni";
+$name="(prijavi se)";
+$page_title="Labaratorijske vežbe"; /*Naslov stranice*/
+
+
+
+
+
+
+?>
